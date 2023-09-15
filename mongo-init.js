28a637a57dc24f54ad5518ecdbf7485ee98c8bdf
jvillegasl <jvillegasl@uni.pe>
2023-09-14 20:23:12 -0500

    prettier config, mui installed

--- mongo-init.js
+++ mongo-init.js
@@ -1,10 +1,10 @@
 db.createUser({
-    user: "test_user",
-    pwd: "test_pass",
-    roles: [
-        {
-            role: "readWrite",
-            db: "javl",
-        },
-    ],
+	user: "test_user",
+	pwd: "test_pass",
+	roles: [
+		{
+			role: "readWrite",
+			db: "javl",
+		},
+	],
 });
