528e1744fa834b173c974cc71f16c4b6d99caad4
jvillegasl <jvillegasl@uni.pe>
2023-09-19 18:09:36 -0500

    feat(auth): config next-auth middleware

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,57 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+	pages: {
+		signIn: "/login",
+		newUser: "/register",
+	},
+	providers: [
+		CredentialsProvider({
+			name: "credentials",
+			credentials: {
+				username: { label: "Username", type: "text" },
+				password: { label: "Password", type: "text" },
+			},
+			async authorize(credentials) {
+				const response = await fetch(
+					`${process.env.NEXTAUTH_URL}/api/login`,
+					{
+						method: "POST",
+						body: JSON.stringify(credentials),
+					},
+				);
+
+				const data = await response.json();
+
+				if (!response.ok) {
+					throw new Error(JSON.stringify(data));
+				}
+
+				const user = data;
+
+				return user;
+			},
+		}),
+	],
+	session: {
+		strategy: "jwt",
+		maxAge: parseInt(process.env.TOKEN_MAX_AGE!),
+	},
+	callbacks: {
+		async session({ session, token }) {
+			session.user = token.user;
+
+			return session;
+		},
+		async jwt({ token, user }) {
+			if (user) token.user = user;
+
+			return token;
+		},
+	},
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
