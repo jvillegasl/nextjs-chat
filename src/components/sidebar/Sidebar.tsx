c97374d26083bdb48c5b5fd5880b0c01376407b6
jvillegasl <jvillegasl@uni.pe>
2023-09-30 13:02:10 -0500

    refactor: Add UserProfile component and removed deprecated code

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,21 +1,17 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
+import { ContactsList } from "./ContactsList";
 import { ConversationsList } from "./ConversationsList";
+import { UserProfile } from "./UserProfile";
 
-type SidebarProps = {};
-
-export async function Sidebar({}: SidebarProps) {
-	const session = await getServerSession(authOptions);
-
-	if (!session) return <h1>Session Not Found</h1>;
-
+export function Sidebar() {
 	return (
 		<div>
 			<h1>Sidebar</h1>
 
-			<pre>{JSON.stringify(session, null, 2)}</pre>
+			<UserProfile />
 
-			<ConversationsList userId={session.user.id} />
+			<ContactsList />
+
+			<ConversationsList />
 		</div>
 	);
 }
