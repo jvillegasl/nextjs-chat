a03602b615258a59e528f294d1ae680f490d3e06
jvillegasl <jvillegasl@uni.pe>
2023-09-30 12:56:35 -0500

    chore: Move provider to layout

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,7 +3,6 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import { dbConnect } from "@/lib";
 import { Sidebar } from "@/components/sidebar";
 import { Conversation } from "@/components/conversation";
-import { ConversationProvider } from "@/providers";
 
 export default async function ChatPage() {
 	await dbConnect();
@@ -13,14 +12,14 @@ export default async function ChatPage() {
 	if (!session) return <h1>User not found</h1>;
 
 	return (
-		<ConversationProvider>
-			<main>
-				<h1>Chat</h1>
-				<div className="grid grid-cols-2">
-					<Sidebar />
-					<Conversation />
-				</div>
-			</main>
-		</ConversationProvider>
+		<main>
+			<h1>Chat</h1>
+
+			<div className="grid grid-cols-2">
+				<Sidebar />
+
+				<Conversation />
+			</div>
+		</main>
 	);
 }
