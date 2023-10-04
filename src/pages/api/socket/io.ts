7c162a70da4a6ef9d2b00163c355949645c48b13
jvillegasl <jvillegasl@uni.pe>
2023-10-04 15:19:50 -0500

    feat: Implement writing event listener and event emitter

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -1,6 +1,9 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Server as NetServer } from "http";
 import { NextApiRequest } from "next";
+import { getServerSession } from "next-auth";
 import { Server as ServerIO } from "socket.io";
+import { parse } from "cookie";
 
 export const config = {
 	api: {
@@ -18,6 +21,33 @@ export default function handler(
 		const io = new ServerIO(httpServer, {
 			path: path,
 			addTrailingSlash: false,
+		});
+
+		io.on("connection", (socket) => {
+			socket.on("chat:writing", async (t) => {
+				const ioRes = {
+					getHeader() {},
+					setCookie() {},
+					setHeader() {},
+				};
+
+				const ioReq = {
+					headers: socket.handshake.headers,
+					cookies: parse(socket.handshake.headers.cookie!),
+				};
+
+				const { user } = (await getServerSession(
+					ioReq as any,
+					ioRes as any,
+					authOptions,
+				))!;
+
+				const event = `${t.conversationId}/chat:writing`;
+
+				socket.broadcast.emit(event, {
+					username: user.username,
+				});
+			});
 		});
 
 		res.socket.server.io = io;
