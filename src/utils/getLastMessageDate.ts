be784db5e61057d0e397ac8f2295ca88c20abe9a
jvillegasl <jvillegasl@uni.pe>
2023-10-19 14:55:51 -0500

    feat: Add util function to formate last message date

---
+++ src/utils/getLastMessageDate.ts
@@ -0,0 +1,18 @@
+import moment from "moment";
+
+export function getLastMessageDate(dateUTC: string) {
+	const date = moment.utc(dateUTC);
+	const currentDate = moment();
+
+	const diffDays = currentDate.diff(date, "days");
+
+	if (diffDays < 1) {
+		return date.format("HH:mm");
+	}
+
+	if (diffDays < 7) {
+		return date.format("dddd");
+	}
+
+	return date.format("DD/MM/YYYY");
+}
