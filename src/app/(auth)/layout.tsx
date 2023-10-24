ea79bf82f2680fae52a9fb34717fd6f18fd33c56
jvillegasl <jvillegasl@uni.pe>
2023-10-24 17:42:21 -0500

    feat: Add redirects to block home always and block register/login if already logged

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,18 @@
 import { ReactNode } from "react";
 import { Box, Container } from "@mui/material";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 type AuthLayoutProps = {
 	children: ReactNode;
 };
 
-export default function AuthLayout({ children }: AuthLayoutProps) {
+export default async function AuthLayout({ children }: AuthLayoutProps) {
+	const session = await getServerSession(authOptions);
+
+	if (session) redirect("/chat");
+
 	return (
 		<Container maxWidth="xs">
 			<Box sx={{ mt: 12 }}>{children}</Box>
